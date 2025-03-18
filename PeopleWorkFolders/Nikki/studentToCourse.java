0c9d6e137587db87ce38a8b83fc4a81e550027d7
Ros_Nikki <roshinikitha@gmail.com>
2025-03-18 09:52:36 -0500

    inital commit

---
+++ PeopleWorkFolders/Nikki/studentToCourse.java
@@ -0,0 +1,24 @@
+/* method only: AddStudentToCourse() and CalculateGrade() 
+
+ */
+
+public class studentToCourse
+{
+    public static void main(String[] args) {
+        
+    }
+
+
+
+    public static void AddStudentToCourse()
+    {
+
+    }
+
+    public static void CalculateGrade()
+    {
+        
+    }
+
+}
+
