25a36e4aa3e91f7a31882b696a635b6a84170c3f
Ros_Nikki <roshinikitha@gmail.com>
2025-03-22 20:36:47 -0500

    post-meeting sync

--- main.java
+++ main.java
@@ -1,18 +1,1 @@
 
-/* FINAL main section with 
-    - 
-    + 
-    + 
- 
- */
-import java.util.Scanner; 
-
-
-public class main {
-    private static Scanner reader = new Scanner(System.in); //makes a scanner for every bit
-
-    public static void main(String[] args) {
-        
-    }
-    
-}
